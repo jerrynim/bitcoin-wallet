d545da03db60292575490008ce30153054cfd7d6
jerrynim <tjerry3@naver.com>
2019-01-31 17:04:41 +0900

    using node js global sharedport

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ getPort().then((port) => {
   const server = bitcoin.app.listen(port, () => {
     console.log(`Running blockchain node on: http://localhost:${port}`);
   });
+  bitcoin.startP2PServer(server);
+  global.sharedPort = port;
 });
 
 const { app, BrowserWindow } = electron;
